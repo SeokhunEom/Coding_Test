62505b384800cc80bc3c540631cdb908bd5d463f
tommya98 <tommya98@naver.com>
2024-03-30 00:28:56 +0900

    백준 5522 solved

---
+++ Baekjoon code/5522.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a, b, c, d, e;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> a >> b >> c >> d >> e;
+  cout << a + b + c + d + e;
+
+  return 0;
+}
