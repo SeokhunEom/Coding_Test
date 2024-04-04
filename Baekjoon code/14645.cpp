7b73e8e1377354e21caac393b4bd9111be3a7d3c
tommya98 <tommya98@naver.com>
2024-04-04 23:00:34 +0900

    백준 14645 solved

---
+++ Baekjoon code/14645.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, K, A, B;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N >> K;
+  for (int i = 0; i < N; i++) cin >> A >> B;
+  cout << "비와이";
+
+  return 0;
+}
