eec4482817962fce9f912aa992c2e7b74d9480c3
tommya98 <tommya98@naver.com>
2024-04-10 13:04:49 +0900

    문제 15. 요세푸스 문제 solved

---
+++ 코딩 테스트 합격자 되기(파이썬)/Chapter07/15.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+
+def solution(N, K):
+    queue = deque(range(1, N + 1))
+
+    while len(queue) > 1:
+        for _ in range(K-1):
+            queue.append(queue.popleft())
+        queue.popleft()
+    return queue[0]
+
+
+# Test
+print(solution(5,2))  # 3
+print(solution(8, 3))  # 7
